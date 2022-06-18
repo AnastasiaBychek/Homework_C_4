90f2c5d06d8fabade74ef673947125d054ae8243
Anastasia Bychek <nastya900063@gmail.com>
2022-06-18 19:28:24 +0300

    Homework_4 files added

---
+++ HW4_Task1/HW4_Task1.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input number A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Method (int x, int y)
+{
+    int result = 0;
+    if(x>0 && y>0) 
+    {
+    result = Convert.ToInt32(Math.Pow(A,B));
+    }
+    return result;  
+}
+Method(A,B);
+Console.WriteLine(Method(A,B));
---
+++ HW4_Task2/HW4_Task2.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input number ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int sumnumbers(int N)
+{
+int sum = 0;
+while (N != 0)
+{
+    sum = sum + N%10;
+    N = N/10;
+}
+return sum;
+}
+sumnumbers(Number);
+Console.WriteLine(sumnumbers(Number));
